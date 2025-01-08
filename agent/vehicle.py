e78212c742521cc9c3d4814c05075abd126e1aba
Ryskann <93831988+Ryskann@users.noreply.github.com>
2025-01-08 16:29:47 +0100

    feat: vehicule agent

---
+++ agent/vehicle.py
@@ -0,0 +1,92 @@
+import csv
+import json
+import paho.mqtt.client as mqtt
+from time import sleep
+
+# CSV file parameters
+SOURCE_FILE = "C:\\Users\\louim\\Downloads\\SubData_10_03_2022.csv"
+LINE_SKIP_AT_START_OF_FILE = 1
+COLUMNS = ['id', 'stationid', 'referenceposition_latitude', 'heading_headingvalue', 'referenceposition_longitude', 'speed_speedvalue', 'referenceposition_altitude_altitudevalue', 'generationtime']
+NEEDED_COLUMNS = ['generationtime', 'id', 'speed_speedvalue', 'referenceposition_latitude', 'referenceposition_longitude']
+
+# Data volume parameter
+DATA_VOLUME = 100000
+
+#MQTT parameters
+BROKER_IP = "localhost"
+BROKER_PORT = 1883
+KEEP_ALIVE = 60
+TOPIC = "traffic/info"
+USER = "user"
+PASSWORD = "password"
+
+def on_connect(client, _userdata, _flags, reason_code, _properties):
+    print(f"Connected with result code {reason_code}")
+    client.subscribe("iot")
+
+def on_message(_client, _userdata, message):
+    print(f"Received message from {message.topic}: {str(message.payload)}")
+
+def get_data_from_row(row, columns):
+    if len(columns) == 0:
+        return row
+    row_data = []
+    for column in columns:
+        row_data.append(row[COLUMNS.index(column)])
+    return row_data
+
+def compute_row(row, columns):
+    row_data = get_data_from_row(row, columns)
+    return row_data
+    
+def get_data(source_file, line_skip = 0, columns = [], number_of_lines = -1):
+    data = []
+    with open(source_file, mode='r', newline='') as file:
+        reader = csv.reader(file)
+        line = 0
+        for row in reader:
+            if line == number_of_lines + line_skip:
+                break
+            line += 1
+            if line <= line_skip:
+                continue
+            data.append(compute_row(row, columns))
+    return data  
+
+def send_row(row, mqtt_client):
+    named_row = {
+        "timestamp" : row[0],
+        "id" : row[1],
+        "speed" : row[2],
+        "latitude" : row[3],
+        "longitude" : row[4]
+    }
+    json_row = json.dumps(named_row)
+    request = mqtt_client.publish(TOPIC, json_row, qos=1)
+    request.wait_for_publish()
+
+def get_MQTT_client():
+    mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_message = on_message
+
+    mqtt_client.username_pw_set(USER, PASSWORD)
+    mqtt_client.connect(BROKER_IP, BROKER_PORT, KEEP_ALIVE)
+
+    mqtt_client.loop_start()
+    return mqtt_client
+
+def send_data(data):
+    mqtt_client = get_MQTT_client()
+    for row in data:
+        send_row(row, mqtt_client)
+    sleep(1)
+    mqtt_client.disconnect()
+
+def main():
+    data = get_data(SOURCE_FILE, LINE_SKIP_AT_START_OF_FILE, NEEDED_COLUMNS, DATA_VOLUME)
+    sorted_data = sorted(data)
+    send_data(sorted_data)
+
+if __name__ == "__main__":
+    main()
